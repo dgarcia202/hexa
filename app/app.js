6452f12c01531765b3d4ca736dfdd6e3ccb1e08f
dgarcia202 <wft2000@gmail.com>
2018-01-25 19:52:32 +0100

    enable static content

--- app/app.js
+++ app/app.js
@@ -7,7 +7,7 @@ const objectRegistry = require('./objectRegistry.js');
 const apiRouter = require('./apiRouter.js');
 
 objectRegistry.update().then(() => {
-  express.static('public');
+  app.use(express.static('public'));
   app.use(bodyParser.json());
   app.use('/api', apiRouter);
   app.listen(configuration.api.port, () => console.log(`HEXA is listening on port ${configuration.api.port}!`));
