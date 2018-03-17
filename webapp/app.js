e48d0978f5f457de541b44e303f2730fadbbf589
dgarcia202 <wft2000@gmail.com>
2018-03-17 19:13:47 +0100

    split menu into its own component

--- webapp/app.js
+++ webapp/app.js
@@ -4,19 +4,20 @@ import Vue from '../node_modules/vue/dist/vue.js'
 import Vuetify from '../node_modules/vuetify/dist/vuetify.js'
 
 import HelloWorldComponent from './components/hello-world.vue'
+import MenuComponent from './components/menu.vue'
 
 Vue.use(Vuetify);
 
 Vue.component('hello-world', HelloWorldComponent);
+Vue.component('hx-menu', MenuComponent);
 
 var app = new Vue({
   el: '#app',
-  data: () => ({
+  data: {
     title: 'HEXA',
     copyright: 'HexaSoft 2018',
-    drawer: null,
-
-  }),
+    drawer: true,
+  },
   props: {
     source: String
   }
---
+++ webapp/components/menu.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-navigation-drawer
+    fixed
+    v-model="isVisible"
+    app
+  >
+    <v-list dense>
+      <v-list-tile @click="">
+        <v-list-tile-action>
+          <v-icon>home</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title>Home - {{ isVisible }}</v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+      <v-list-tile @click="">
+        <v-list-tile-action>
+          <v-icon>contact_mail</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title>Contact</v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  props: ['isVisible'],
+  data() {
+    return {}
+  }
+}
+</script>
