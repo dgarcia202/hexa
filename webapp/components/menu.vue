6b9e0a7c2ad5e091476df00f0cd0f32fc13b5381
dgarcia202 <wft2000@gmail.com>
2018-03-17 21:42:57 +0100

    add servcie dependencies as mixins

--- webapp/components/menu.vue
+++ webapp/components/menu.vue
@@ -10,26 +10,36 @@
           <v-icon>home</v-icon>
         </v-list-tile-action>
         <v-list-tile-content>
-          <v-list-tile-title>Home - {{ isVisible }}</v-list-tile-title>
+          <v-list-tile-title>Home</v-list-tile-title>
         </v-list-tile-content>
       </v-list-tile>
-      <v-list-tile @click="">
+
+      <v-list-tile v-for="resource in resources" :key="resource.name" @click="">
         <v-list-tile-action>
-          <v-icon>contact_mail</v-icon>
+          <v-icon>widgets</v-icon>
         </v-list-tile-action>
         <v-list-tile-content>
-          <v-list-tile-title>Contact</v-list-tile-title>
+          <v-list-tile-title>{{ resource.name }}</v-list-tile-title>
         </v-list-tile-content>
       </v-list-tile>
+
     </v-list>
   </v-navigation-drawer>
 </template>
 
 <script>
+import SchemaService from '../services/schema.service'
+
 export default {
+  mixins: [SchemaService],
   props: ['isVisible'],
   data() {
-    return {}
+    return {
+      resources: []
+    }
+  },
+  created() {
+    this.resources = this.getAllDefinitions();
   }
 }
 </script>
---
+++ webapp/services/schema.service.js
@@ -0,0 +1,7 @@
+export default {
+  methods: {
+    getAllDefinitions: () => {
+      return [{ name: 'hola' }, { name: 'adios' }, { name: 'lalal' }];
+    }
+  }
+}
