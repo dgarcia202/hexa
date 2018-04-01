b4f27e9c69cc54d5254608781ff97b169c311d43
diego <wft2000@gmail.com>
2018-04-01 19:15:18 +0200

    added data table for search component

--- webapp/components/search-resource.vue
+++ webapp/components/search-resource.vue
@@ -1,10 +1,29 @@
 <template>
-  <v-layout>
+  <v-layout row wrap align-content-start>
     <v-flex xs6>
-        <v-text-field solo append-icon="search"></v-text-field>
+        <v-text-field mb-3 solo append-icon="search" v-model="searchText" @change="updateResults"></v-text-field>
     </v-flex>
+    <v-flex xs12>
+        <v-data-table
+            :headers="headers"
+            :items="items"
+            hide-actions
+            class="elevation-1"
+        >
+            <template v-for="item in items">
+                <td :key="item.id" >{{ item.name }}</td>
+                <td :key="item.id" class="text-xs-right">{{ item.description }}</td>
+                <td :key="item.id" class="text-xs-right">{{ item.dateAdded }}</td>
+            </template>
+        </v-data-table>
+
+    </v-flex>    
   </v-layout>
 </template>
+
+
+
+
 
 <script>
 import SchemaService from '../services/schema.service'
@@ -14,7 +33,23 @@ export default {
   props: ['id'],
   data() {
     return {        
+        searchText: null,
+        headers: [
+            { text: 'Name', value: 'name' },
+            { text: 'Description', value: 'description' },
+            { text: 'Date added', value: 'dateAdded' }
+        ],
+        items: []
     }
+  },
+  methods: {
+      updateResults() {
+        this.items.push({
+            name: 'diego',
+            description: 'lalala',
+            dateAdded: '2018-02-01 12:35:26.843'
+        });
+      }
   }
 }
 </script>
